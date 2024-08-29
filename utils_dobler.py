db850d9a8cd4d6d67e0a3ab8676d56911162a6ba
evandobler98 <S576852@nwmissouri.edu>
2024-08-29 14:57:15 -0500

    Finalize utils module

--- utils_dobler.py
+++ utils_dobler.py
@@ -1,68 +1,24 @@
-''' ITERATION 4
+''' FINAL Iteration
 
-Module: Dobler Data - Reusable Module for My Data Analytics Projects
+Module: Dobler Data: Delivering Baseball Insights
 
 This module provides a simple, reusable foundation for my analytics projects. 
-
-Process:
-In this fourth iteration, I introduce some basic statistics using Python.
-    - min() is a built in function to find the smallest value passed in
-    - max() is a built in function to find the largest value passed in
-    - The statistics module offers methods to calculate mean and standard deviation.
 '''
-#####################################
-# Import Modules at the Top
-#####################################
-
-# In Python, we can import modules to add extra tools and functions.
-# Below, we're importing:
-# - 'statistics': This gives us tools to calculate things like averages.
-# Use CTRL F and type statistics to see where it is used in the code. 
-# Did you find statistics.mean()?
-# Did you find statistics.stdev()?
 
 import statistics
 
-#####################################
-# Declare global variables 
-#####################################
 
-# Boolean variable to indicate if the company has internation clients
 has_international_clients: bool = True
-
-# Boolean variable to indicate multiple languages offered
 multiple_languages_offered: bool = True
-
-# Integer variable for the number of years in operation
 years_in_operation: int = 7
-
-# Integer variable for amount of players per session
 players_per_session: int = 4
-
-# List of strings representing the skills offered by the company
 skills_offered: list = ["Strength", "Agility", "Flexibility", "Speed"]
-
-#List of strings representing highest grossing locations
 highest_grossing_locations : list = ["Kansas City", "Dallas", "Miami", "Phoenix"]
-
-# List of floats representing individual client satisfaction scores
 client_satisfaction_scores: list = [4.2, 5.0, 4.8, 4.9, 4.9]
-
-# List of floats representing top bat speeds before training
 top_bat_speeds_before_training: list = [59.8, 62.4, 61.8, 63.4, 64.1]
-
-# List of floats representing top bat speeds after training
 top_bat_speeds_after_training: list = [61.2, 63.6, 62.9, 65.1, 65.7]
-
-# List of floats representing positive changes in bat speeds
 top_bat_speeds_change: list = [1.4, 1.2, 1.1, 1.7, 1.6]
 
-#####################################
-# Calculate Basic Statistics 
-#   Do this BEFORE we declare the byline
-#   so the values have been calculated
-#   and are ready for use in the byline.
-#####################################
 
 #Calculate basic stats using built-in functions min(), max(), and statistics module functions mean() and stdev()
 min_score: float = min(client_satisfaction_scores)
@@ -83,10 +39,8 @@ stdev_bat_speed_after: float = statistics.stdev(top_bat_speeds_after_training)
 mean_bat_speed_change: float = statistics.mean(top_bat_speeds_change)
 stdev_bat_speed_change: float = statistics.stdev(top_bat_speeds_change)
 
-#####################################
+
 # Declare a global variable named byline.
-# Make it a multiline f-string to show our information.
-#####################################
 
 byline: str = f"""
 ---------------------------------------------------
@@ -121,26 +75,14 @@ Mean Bat Speed Change:                   {mean_bat_speed_change:.2f}
 Standard Deviation of Bat Speed Change:  {stdev_bat_speed_change:.2f}
 """
 
-#####################################
-# Define the get_byline() Function
-#####################################
 
 def get_byline() -> str:
     '''Return a byline for my analytics projects.'''
     return byline 
 
-#####################################
-# Define a main() function for this module.
-#####################################
-
-# The main function now calls get_byline() to retrieve the byline.
 def main() -> None:
     '''Print the byline to the console when this function is called.'''
     print(get_byline())
-
-#####################################
-# Conditional Execution 
-#####################################
 
 if __name__ == '__main__':
     main()
