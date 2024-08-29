94eac1c1bfc6f77e2dd0c5c5621126c6fbf241dc
evandobler98 <S576852@nwmissouri.edu>
2024-08-29 13:50:01 -0500

    Iteration 3 - multiline byline

--- utils_dobler.py
+++ utils_dobler.py
@@ -1,28 +1,69 @@
-''' ITERATION 2
+''' ITERATION 3
 
 Module: Dobler Data - Reusable Module for My Data Analytics Projects
 
 This module provides a simple, reusable foundation for my analytics projects. 
 
 Process:
-
-In this second iteration, add a function that returns the byline as a string.
-I'll create a function named get_byline().
-It'll return my byline to whatever calls the get_byline() function.
-I'll update the main() function to use the new get_byline() function.
-
-Same conditional boilerplate at the end.
-
-I'll test this version before adding more code that shows:
-- my ability to declare variables of different types 
-- my ability to use Python to calculate basic descriptive statistics.
+In this third iteration, I declare additional variables to show skills with different data types.
 '''
 
 #####################################
-# Declare a global variable named byline.
+# Declare global variables - keep byline at the end 
+# We will use this information in a smarter byline 
 #####################################
 
-byline: str = 'Dobler Data: Delivering Baseball Insights'
+# Boolean variable to indicate if the company has internation clients
+has_international_clients: bool = True
+
+# Boolean variable to indicate multiple languages offered
+multiple_languages_offered: bool = True
+
+# Integer variable for the number of years in operation
+years_in_operation: int = 7
+
+# Integer variable for amount of players per session
+players_per_session: int = 4
+
+# List of strings representing the skills offered by the company
+skills_offered: list = ["Strength", "Agility", "Flexibility", "Speed"]
+
+#List of strings representing highest grossing locations
+highest_grossing_locations : list = ["Kansas City", "Dallas", "Miami", "Phoenix"]
+
+# List of floats representing individual client satisfaction scores
+client_satisfaction_scores: list = [4.2, 5.0, 4.8, 4.9, 4.9]
+
+# List of floats representing top bat speeds before training
+top_bat_speeds_before_training: list = [59.8, 62.4, 61.8, 63.4, 64.1]
+
+# List of floats representing top bat speeds after training
+top_bat_speeds_after_training: list = [61.2, 63.6, 62.9, 65.1, 65.7]
+
+# List of floats representing positive changes in bat speeds
+top_bat_speeds_change: list = [1.4, 1.2, 1.1, 1.7, 1.6]
+
+#####################################
+# Declare a global variable named byline.
+# Make it a multiline f-string to show our information.
+#####################################
+
+byline: str = f"""
+---------------------------------------------------
+Dobler Data: Delivering Baseball Insights
+---------------------------------------------------
+Has International Clients:      {has_international_clients}
+Years in Operation:             {years_in_operation}
+Highest Grossing Locations:     {highest_grossing_locations}
+Has Multiple Languages Offered: {multiple_languages_offered}
+Skills Offered:                 {skills_offered}
+Client Satisfaction Scores:     {client_satisfaction_scores}
+Players Per Session:            {players_per_session}
+Top Bat Speeds Before Training: {top_bat_speeds_before_training}
+Top Bat Speeds After Training:  {top_bat_speeds_after_training}
+Top Bat Speeds Change:          {top_bat_speeds_change}
+
+"""
 
 #####################################
 # Define the get_byline() Function
